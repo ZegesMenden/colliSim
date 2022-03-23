f8e806f53a31cc76ef2b02152b920d9eedab2073
ZegesMenden <51548005+ZegesMenden@users.noreply.github.com>
2022-03-23 15:29:29 -0400

    more core functions
    added string, rotation around point, and fixed multiplication by float

--- core/physics.py
+++ core/physics.py
@@ -15,6 +15,9 @@ class vector2:
         self.x = x
         self.y = y
 
+    def __str__(self) -> str:
+        return(f'{self.x}, {self.y}')
+
     def __add__(self, other) -> vector2:
 
         if isinstance(other, vector2):
@@ -35,7 +38,7 @@ class vector2:
             return vector2(self.x * other.x, self.y * other.y)
         else:
             if isinstance(other, float) or isinstance(other, int):
-                return vector2(self.x / other, self.y / other)
+                return vector2(self.x * other, self.y * other)
             else:
                 raise NotImplementedError
     
@@ -55,5 +58,9 @@ class vector2:
     def norm(self) -> vector2:
         return self / self.magnitude()
 
-def rotate_vector_around_point(vec: vector2, rotation: float):
-    return vector2(vec.x * np.cos(rotation) - vec.y * np.sin(rotation), vec.x * np.sin(rotation) + vec.y * np.cos(rotation))
+    def cross(self, other: vector2) -> float:
+        return (self.x*other.y) - (self.y*other.x)
+
+def rotate_vector_around_point(vec: vector2, rotation: float, point: vector2 = vector2(0, 0)) -> vector2:
+    tmp: vector2 = vec - point
+    return vector2((tmp.x * np.cos(rotation) - tmp.y * np.sin(rotation)) + point.x, (tmp.x * np.sin(rotation) + tmp.y * np.cos(rotation)) + point.y)
